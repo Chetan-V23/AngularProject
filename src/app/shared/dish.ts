3888b004b247e2b66a14388341fae48b76b1559e
Chetan Vallish <chetanvallish@gmail.com>
2021-05-12 00:14:32 +0530

    Components part 1

---
+++ src/app/shared/dish.ts
@@ -0,0 +1,24 @@
+
+
+export class Dish {
+    id: string;
+    name: string;
+    image:string;
+    category:string;
+    featured:boolean;
+    label:string;
+    price:string;
+    description:string;
+
+    constructor(){
+        this.id="";
+        this.name="";
+        this.image="";
+        this.category="";
+        this.featured=false;
+        this.image="";
+        this.label="";
+        this.price="";
+        this.description="";
+    }
+}
