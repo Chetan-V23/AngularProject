9f4b5d447e617c7e6bf23c7e6d84355477d77988
Chetan Vallish <chetanvallish@gmail.com>
2021-06-10 01:47:14 +0530

    reactive forms validation

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
+import { of, Observable } from 'rxjs';
 import {Leader } from '../shared/leader';
 import {LEADERS} from '../shared/leaders';
+import {delay} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +10,12 @@ import {LEADERS} from '../shared/leaders';
 export class LeaderService {
 
   constructor() { }
-  getLeaders():Promise<Leader[]>{
-    return Promise.resolve(LEADERS);
+  getLeaders():Observable<Leader[]>{
+    return of(LEADERS).pipe(delay(2000));
   }
 
-  getFeaturedLeader( ):Promise<Leader>{
-    return Promise.resolve(LEADERS.filter((ldr)=>(ldr.featured))[0]);
+  getFeaturedLeader( ):Observable<Leader>{
+    //return Promise.resolve(LEADERS.filter((ldr)=>(ldr.featured))[0]);
+    return of(LEADERS.filter((ldr)=>(ldr.featured))[0]);
   }
 }
--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Promotion } from '../shared/promotion';
 import { PROMOTIONS } from '../shared/promotions';
-
+import { Observable, observable, of } from 'rxjs';
+import {delay, filter} from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,13 +10,16 @@ export class PromotionService {
 
   constructor() { }
 
-  getPromotions(): Promise<Promotion[]> {
-    return Promise.resolve(PROMOTIONS);
+  getPromotions(): Observable<Promotion[]> {
+   // return Promise.resolve(PROMOTIONS);
+    return of(PROMOTIONS).pipe(delay(2000));
   }
-  getPromotion(id: string): Promise<Promotion> {
-    return Promise.resolve(PROMOTIONS.filter((promo) => ( promo.id === id ))[0]);
+  getPromotion(id: string): Observable<Promotion> {
+    //return Promise.resolve(PROMOTIONS.filter((promo) => ( promo.id === id ))[0]);
+    return of(PROMOTIONS.filter((promo)=>(promo.id==id))[0]);
   }
-  getFeaturedPromotion(): Promise<Promotion> {
-    return Promise.resolve(PROMOTIONS.filter((promo) => ( promo.featured === true ))[0])
+  getFeaturedPromotion(): Observable<Promotion> {
+    //return Promise.resolve(PROMOTIONS.filter((promo) => ( promo.featured === true ))[0])
+  return of(PROMOTIONS.filter((promo)=>(promo.featured==true))[0]);
   }
 }
